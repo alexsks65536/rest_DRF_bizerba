be71500233342ad2ced93fd797ef719f30291e4a
alexsks65536 <megbyte@mail.ru>
2023-05-16 23:22:49 +0300

    create Routers, ViewSet

--- bizerba/bizerba/urls.py
+++ bizerba/bizerba/urls.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from mainapp.views import JobAPIView, JobAPIUpdate, JobAPIDetailView
+# from mainapp.views import JobAPIView, JobAPIUpdate, JobAPIDetailView, SparePartAPIDetailView, SparePartAPIView
+from mainapp.views import JobViewSet, SparePartViewSet, CustomerViewSet, DepartmentViewSet, ScaleModelViewSet, \
+    ScaleViewSet, ReceivingViewSet, InstallationViewSet
+
+router = routers.DefaultRouter()
+router.register(r'job', JobViewSet)  # заявки в работу
+router.register(r'spare', SparePartViewSet)  # запчасти с артикулами
+router.register(r'customer', CustomerViewSet)
+router.register(r'department', DepartmentViewSet)
+router.register(r'scalemodel', ScaleModelViewSet)
+router.register(r'scale', ScaleViewSet)
+router.register(r'receiving', ReceivingViewSet)
+router.register(r'installation', InstallationViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/job/', JobAPIView.as_view()),
-    path('api/v1/job/<int:pk>', JobAPIUpdate.as_view()),
-    path('api/v1/jobdetail/<int:pk>', JobAPIDetailView.as_view())
+    path('api/v1/', include(router.urls)),
+    # path('api/v1/job/', JobAPIView.as_view()),  # заявки в работу
+    # path('api/v1/job/<int:pk>', JobAPIUpdate.as_view()),  # заявки в работу
+    # path('api/v1/jobdetail/<int:pk>', JobAPIDetailView.as_view()),  # заявки в работу
+    # path('api/v1/sparepart/', SparePartAPIView.as_view()),  # запчасти с артикулами
+    # path('api/v1/sparedetail/<int:pk>', SparePartAPIDetailView.as_view()),  # запчасти с артикулами
 ]
--- bizerba/mainapp/views.py
+++ bizerba/mainapp/views.py
@@ -1,20 +1,70 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import JobApplicationSerializer
-from .models import *
+from rest_framework import generics, viewsets
+from .serializers import JobApplicationSerializer, SparePartSerializer, CustomerSerializer, DepartmentSerializer, \
+    ScaleModelSerializer, ScaleSerializer, ReceivingSerializer, InstallationSerializer
+from .models import JobApplication, SparePart, Customer, Department, ScaleModel, Scale, Receiving, Installation
 
 
-class JobAPIView(generics.ListCreateAPIView):
+class JobViewSet(viewsets.ModelViewSet):  # список работ
     queryset = JobApplication.objects.all()
     serializer_class = JobApplicationSerializer
 
 
-class JobAPIUpdate(generics.UpdateAPIView):
-    queryset = JobApplication.objects.all()
-    serializer_class = JobApplicationSerializer
+class SparePartViewSet(viewsets.ModelViewSet):  # список ЗИП
+    queryset = SparePart.objects.all()
+    serializer_class = SparePartSerializer
 
 
-class JobAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = JobApplication.objects.all()
-    serializer_class = JobApplicationSerializer
+class CustomerViewSet(viewsets.ModelViewSet):  # заказчики
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
 
+
+class DepartmentViewSet(viewsets.ModelViewSet):  # Отделы
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class ScaleModelViewSet(viewsets.ModelViewSet):  # Модели весов
+    queryset = ScaleModel.objects.all()
+    serializer_class = ScaleModelSerializer
+
+
+class ScaleViewSet(viewsets.ModelViewSet):  # Список весов
+    queryset = Scale.objects.all()
+    serializer_class = ScaleSerializer
+
+
+class ReceivingViewSet(viewsets.ModelViewSet):  # Приход ЗИП
+    queryset = Receiving.objects.all()
+    serializer_class = ReceivingSerializer
+
+
+class InstallationViewSet(viewsets.ModelViewSet):  # Установка ЗИП
+    queryset = Installation.objects.all()
+    serializer_class = InstallationSerializer
+
+
+# class JobAPIView(generics.ListCreateAPIView):  # заявки в работу
+#     queryset = JobApplication.objects.all()
+#     serializer_class = JobApplicationSerializer
+#
+#
+# class JobAPIUpdate(generics.UpdateAPIView):  # заявки в работу
+#     queryset = JobApplication.objects.all()
+#     serializer_class = JobApplicationSerializer
+#
+#
+# class JobAPIDetailView(generics.RetrieveUpdateDestroyAPIView):  # заявки в работу
+#     queryset = JobApplication.objects.all()
+#     serializer_class = JobApplicationSerializer
+#
+#
+# class SparePartAPIView(generics.ListCreateAPIView):  # запчасти с артикулами
+#     queryset = SparePart.objects.all()
+#     serializer_class = SparePartSerializer
+#
+#
+# class SparePartAPIDetailView(generics.RetrieveUpdateDestroyAPIView):  # запчасти с артикулами
+#     queryset = SparePart.objects.all()
+#     serializer_class = SparePartSerializer
